18032fca5077c5ade126a2c5ad0cb6221d5a8855
Mathilde Loft <mathildeloft@computer-2.eduroam.wireless.dtu.dk>
2019-01-09 16:37:43 +0100

    new

--- matmult_nmk.c
+++ matmult_nmk.c
@@ -2,8 +2,8 @@
 void matmult_nmk(int m, int n, int k, double **A, double **B, double **C) {
   int c, d, e;
 
-  for (d = 0; d < n; e++) {
-        for (c = 0; c < m; d++) {
+  for (d = 0; d < n; d++) {
+        for (c = 0; c < m; c++) {
           C[c][d] = 0;
         }
       }
