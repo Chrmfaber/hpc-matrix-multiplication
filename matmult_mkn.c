da3e17eb90fbb92ba83a3a9c27cf8d29793054a4
Hedda Berg <berg.hedda@gmail.com>
2019-01-09 15:58:58 +0100

    no message

--- matmult_mkn.c
+++ matmult_mkn.c
@@ -1,17 +1,13 @@
 
-void
-matmult_mkn(int m,int n,int k,double **A,double **B,double **C){
-  int c,d,e;
-  double sum=0;
+void matmult_mkn(int m, int n, int k, double **A, double **B, double **C) {
+  int c, d, e;
 
   for (c = 0; c < m; c++) {
-        for (d = 0; d < k; d++) {
-          for (e = 0; e < n; e++) {
-            sum += A[c][e]*B[e][d];
+        for (e = 0; e < k; e++) {
+          C[c][d] = 0;
+          for (d = 0; d < n; d++) {
+            C[c][d] += A[c][e]*B[e][d];
           }
-
-          C[c][d] = sum;
-          sum = 0;
         }
       }
 }
