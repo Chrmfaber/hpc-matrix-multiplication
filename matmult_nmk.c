3dfbfcf376955c13f21f96c9c90a9226ff22c2fa
s144278 <46522824+s144278@users.noreply.github.com>
2019-01-09 12:38:22 +0100

    Add files via upload
    Hello

--- matmult_nmk.c
+++ matmult_nmk.c
@@ -4,8 +4,8 @@ matmult_nat(int m,int n,int k,double **A,double **B,double **C){
   int c,d,e;
   double sum=0;
 
-  for (c = 0; c < m; c++) {
-        for (d = 0; d < n; d++) {
+  for (c = 0; c < n; c++) {
+        for (d = 0; d < m; d++) {
           for (e = 0; e < k; e++) {
             sum += A[c][e]*B[e][d];
           }
