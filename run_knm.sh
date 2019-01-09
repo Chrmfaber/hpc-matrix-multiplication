a7ac478f29e93c7e9688dd9fc38ec78f4363a108
s144278 <s144278@student.dtu.dk>
2019-01-09 15:31:50 +0100

    usefull data for plots

--- run_knm.sh
+++ run_knm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NMK="10 100 1000 2000"
+NMK="10 40 70 100 150 200 250 300"
 TYPE="nat lib knm kmn mnk mkn nkm nmk"
 LOGEXT=dat
 /bin/rm -f O3nat.$LOGEXT O3lib.$LOGEXT O3knm.$LOGEXT O3kmn.$LOGEXT O3mnk.$LOGEXT O3mkn.$LOGEXT O3nkm.$LOGEXT O3nmk.$LOGEXT
