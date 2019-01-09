f53cf168974165a73d1ef66f06c8c0f139855829
Mathilde Loft <mathildeloft@computer-2.eduroam.wireless.dtu.dk>
2019-01-09 16:29:16 +0100

    new

--- matmult_nkm.c
+++ matmult_nkm.c
@@ -2,9 +2,14 @@
 void matmult_nkm(int m, int n, int k, double **A, double **B, double **C) {
   int c, d, e;
 
+  for (d = 0; d < n; e++) {
+        for (c = 0; c < m; d++) {
+          C[c][d] = 0;
+        }
+      }
+
   for (d = 0; d < n; d++) {
         for (e = 0; e < k; e++) {
-          C[c][d] = 0;
           for (c = 0; c < m; c++) {
             C[c][d] += A[c][e]*B[e][d];
           }
