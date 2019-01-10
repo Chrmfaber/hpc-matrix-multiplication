84c37286ad78910dcc73ec543a67029a1aa79218
mshPolo <marcus1x@live.dk>
2019-01-10 19:43:29 +0100

    updated makefiles

--- matmult_blk.c
+++ matmult_blk.c
@@ -35,7 +35,7 @@ void matmult_blk(int m, int n, int k, double **A, double **B, double **C,
     for (e1 = 0; e1 < k; e1 += bs) {
       for (j1 = 0; j1 < n; j1 += bs) {
         for (i2 = 0; i2 < MIN(m - i1, bs); i2++) {
-	          for (e2 = 0; e2 < MIN(k - e1, bs); e2++) {
+	       for (e2 = 0; e2 < MIN(k - e1, bs); e2++) {
             for (j2 = 0; j2 < MIN(n - j1, bs); j2++) {
 	       C[i1 + i2][j1 + j2] += A[i1 + i2][e1 + e2] * B[e1 + e2][j1 + j2];
 	    }
