21c89caeb60955920ee6516ee348e816a0bf353b
Hedda Berg <berg.hedda@gmail.com>
2019-01-10 11:23:28 +0100

    no message

--- gcc/plot_gcc.R
+++ gcc/plot_gcc.R
@@ -10,6 +10,9 @@ source('gcc_NoOpt.R')
 # Line plot with multiple groups
 
 gcc_O1_data_blk$V5 <- as.factor(gcc_O1_data_blk$V5)
-ggplot(data=gcc_O1_data_blk, aes(x=V1, y=V2, col=V5)) + labs(x="Memory usage (kbytes)", y = "Mflop/s") +
+gcc_O1_data_blk$size <- interaction( "O1", gcc_O1_data_blk$V5, sep = ":")
+ggplot(data=gcc_O1_data_blk, aes(x=V1, y=V2, col=size)) + labs(x="Memory usage (kbytes)", y = "Mflop/s") +
   geom_line()+
   geom_point()+labs(colour = "Block size")
+
+
