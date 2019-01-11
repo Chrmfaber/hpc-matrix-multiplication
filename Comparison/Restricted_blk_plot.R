b7ede032618a1a505ddbd14712379228759a95a7
Hedda Berg <berg.hedda@gmail.com>
2019-01-11 15:42:08 +0100

    no message

--- Comparison/Restricted_blk_plot.R
+++ Comparison/Restricted_blk_plot.R
@@ -21,8 +21,13 @@ suncc_restrict <- suncc_restrict_mkn_order_blockingmkn[c(1:2)]
 suncc_restrict$TYPE <- "sun"
 suncc_restrict$V5 <- c(rep(0,17))
 
-res = rbind(sun_restrict_256,gcc_restrict_256,gcc_restrict,suncc_restrict)
+gcc_RestrictFlags_lib_order_blockinglib <- read.table("~/Google Drive/10.semester/02614 HPC/Assignment 1/GIT/DataLibFastest/gcc_RestrictFlags_lib_order_blockinglib.dat", quote="\"", comment.char="")
+gcc_restrict_lib <- gcc_RestrictFlags_lib_order_blockinglib[c(1:2)]
+gcc_restrict_lib$TYPE <- "lib"
+gcc_restrict_lib$V5 <- c(rep(0,17))
+
+res = rbind(sun_restrict_256,gcc_restrict_256,gcc_restrict,suncc_restrict,gcc_restrict_lib)
 
 ggplot(data=res, aes(x=V1, y=V2, col=TYPE)) + labs(x="Memory usage (kbytes)", y = "Mflop/s") +
-  geom_line()+
+  geom_line()+ylim(0,15000)+
   geom_point()+labs(colour = "")
